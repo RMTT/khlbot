576d79825c94e384af1611281978d59c4979288d
RMT <d.rong@outlook.com>
2021-04-29 18:49:49 +0800

    packet & release to pypi

--- khlbot/__init__.py
+++ khlbot/__init__.py
@@ -1,3 +1,7 @@
+from khlbot.core.Bot import Bot
+from khlbot.core.Logger import Logger
+from khlbot.core.Commander import Commander
+
 # 以下是开黑啦消息类型常量
 KHL_MSG_TEXT = 1
 KHL_MSG_CARD = 10
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="khlbot",
+    version="0.1",
+    author="RMT",
+    author_email="d.rong@outlook.com",
+    description="Robot framework for KaiHeiLa",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RMTT/khlbot",
+    packages=setuptools.find_packages(),
+    install_requires=['requests', 'websockets'],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
