28f6c6397f2571a3c6d7be1b6f6ee386731aa772
RMT <d.rong@outlook.com>
2021-05-09 08:36:22 +0800

    v0.21

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="khlbot",
-    version="0.2",
+    version="0.21",
     author="RMT",
     author_email="d.rong@outlook.com",
     description="Robot framework for KaiHeiLa",
